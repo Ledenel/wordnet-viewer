3d242d37a36f72502b0c88a3e3c3d2349c032090
ledenel <ledenelintelli@gmail.com>
2021-03-06 06:31:16 +0800

    add omw

--- init.py
+++ init.py
@@ -11,3 +11,4 @@ if __name__ == "__main__" or not os.path.exists(os.path.expanduser("~/nltk_data/
         ssl._create_default_https_context = _create_unverified_https_context
 
     nltk.download("wordnet")
+    nltk.download("omw")
